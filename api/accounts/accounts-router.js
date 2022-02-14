dcaa7e4cd82bc08bbfccb45b06a334ad9dc40d0a
Jorge <jorgeevangelista500@gmail.com>
2022-02-14 16:39:04 -0500

    added getAll functionality

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,7 +1,12 @@
+
 const router = require('express').Router()
+const Accounts = require('./accounts-model')
 
 router.get('/', (req, res, next) => {
-  // DO YOUR MAGIC
+  Accounts.getAll()
+    .then(accounts =>{
+      res.status(200).json(accounts)
+    })
 })
 
 router.get('/:id', (req, res, next) => {
